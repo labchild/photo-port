23a325b4e7c3ac7bc2ce6f9cf8a175a1d19e4d03
l bates childs <labchilds@gmail.com>
2022-03-10 01:39:40 -0500

    add modal and open modal with assets

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import Modal from '../Modal';
+import Modal from '../Modal';
 
 const PhotoList = ({ category }) => {
 
@@ -100,17 +100,26 @@ const PhotoList = ({ category }) => {
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricie',
     },
   ]);
+  const [currentPhoto, setCurrentPhoto] = useState();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
+  const toggleModal = (image, i) => {
+    // current photo
+    setCurrentPhoto({...image, index: i});
+    setIsModalOpen(true);
+  }
 
   return (
     <div>
+      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
       <div className="flex-row">
         {currentPhotos.map((image, i) => (
           <img
             src={require(`../../assets/small/${category}/${i}.jpg`)}
             alt={image.name}
             className="img-thumbnail mx-1"
+            onClick={() => toggleModal(image, i)}
             key={image.name}
           />
         ))}
